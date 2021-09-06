7bf559ba7ecfe1846735c305173c9574cf806879
XG-2580 <limsathya1234@gmail.com>
2021-09-06 20:09:24 +0700

    added

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const { Client, Collection } = require('discord.js');
+const { token, prefix } = require('./config.json');
+const client = new Client({
+	restTimeOffset: 0,
+	allowedMentions: {
+		parse: [ "roles", "users", "everyone" ],
+		repliedUser: false,
+	},
+	partials: [ "MESSAGE", "CHANNEL", "REACTION" ],
+	intents: [ 'GUILDS', 'GUILD_MESSAGES' ]
+})
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply(`${client.ws.ping}ms`);
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.login(token);
